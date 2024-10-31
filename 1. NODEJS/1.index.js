9926c0f105d9664f24e054b0e9d968aeacbddc30
Andruss9411 <andres.salinas9411@gmail.com>
2024-10-30 21:04:28 -0500

    Agrega nueva libreria

--- 1. NODEJS/1.index.js
+++ 1. NODEJS/1.index.js
@@ -1,5 +1,9 @@
-/*  1-> MODULOS NATIVOS DE NODE JS */
+/*  
+   1-> MODULOS NATIVOS DE NODE JS 
+*/
+
 import {platform, release, arch, cpus, freemem, totalmem} from "node:os";
+const {hash} = await import("node:crypto");
 
 console.log("Sistema operativo: ",platform());
 console.log("Release: ",release());
@@ -7,3 +11,5 @@ console.log("Arquitectura: ",arch());
 console.log("CPUs: ",cpus());
 console.log("Memoria libre: ",freemem());
 console.log("Memoria total: ",totalmem());
+
+console.log("hash: ", hash.digest("Ack3rman*"));
---
+++ 1. NODEJS/2.index.js
@@ -0,0 +1,5 @@
+/*
+  2 -> MODULOS DE TERCEROS
+*/
+
+
